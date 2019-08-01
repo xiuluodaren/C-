14423c2995e432f586e864d067bea94ed32f001d
修罗 <1448564807@qq.com>
2019-08-01 22:31:36 +0800

    数组

--- ArrayDemo.cpp
+++ ArrayDemo.cpp
@@ -9,8 +9,31 @@ using namespace std;
 int main()
 {
     double a[] = {1.1,4.4,3.3,2.2}, b[4];
+
+    //原样输出
     copy(a, a+4, ostream_iterator<double>(cout,","));
     cout << endl;
+
+    //逆向输出
+    reverse_copy(a, a+4, ostream_iterator<double>(cout," "));
+    cout << endl;
+
+    //原样复制到b
+    copy(a, a + 4, b);
+    copy(b, b+4, ostream_iterator<double>(cout,","));
+    cout << endl;
+
+    //对数组a升幂排序
+    sort(a, a + 4);
+    copy(a, a + 4, ostream_iterator<double>(cout," "));
+    cout << endl;
+
+    //将a逆向复制到b
+    reverse_copy(a, a + 4, b);
+    copy(b, b + 4,ostream_iterator<double>(cout," "));
+    cout << endl;
+
+
 
     return 0;
 }
