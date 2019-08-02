eee3e32737c24148fbc87137dbcf53586e022994
修罗大人 <14485564807@qq.com>
2019-08-02 13:12:21 +0800

    类

---
+++ part2/ClassDemo.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Point{
+    private :
+        double x, y;
+
+    public:
+        Point(){};
+        Point(double a, double b)
+        {x = a, y = b;}
+        
+        void Setxy(double a, double b)
+        {x = a, y = b;}
+        
+        void Display()
+        {cout << x << "\t" << y << endl;}
+
+};
+
+int main()
+{
+    Point a;
+    Point b(18.5, 10.6);
+    a.Setxy(10.6, 18.5);
+
+    a.Display();
+    b.Display();
+
+    return 0;
+}
+
