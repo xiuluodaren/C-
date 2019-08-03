42a6e09eae24951153f8021917170ece3dbde0dc
修罗大人 <14485564807@qq.com>
2019-08-03 08:52:24 +0800

    StringDemo

--- part2/StringDemo.cpp
+++ part2/StringDemo.cpp
@@ -1,8 +1,12 @@
 #include <string>
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <iterator>
+
 using namespace std;
 
-int main()
+void test1()
 {
     string str1("We are here!");
     string str2 = "We are you?";
@@ -11,9 +15,54 @@ int main()
     cout << "please input a word:";
     cin >> str1;
     cout << "length of the " << str1 << " is " << str1.size() << "." << endl;
-
-    system("pause");  
-
-    return 0;
 }
 
+void test2()
+{
+    string str1 = "we are here!", str2 = str1;
+    reverse(&str1[0], &str1[0] + 12);
+    cout << str1 << endl;
+
+    copy(&str1[0], &str1[0] + 12, &str2[0]);
+    cout << str2 << endl;
+
+    reverse_copy(&str2[0], &str2[0] + 12, ostream_iterator<char>(cout));
+    cout << endl;
+
+}
+
+void test3()
+{
+    string str1 = "wearehere!", str2(str1);
+    reverse(str1.begin(), str1.end());
+    cout << "str1: " << str1 << endl;
+
+    copy(str1.begin(), str1.end(), str2.begin());
+    sort(str1.begin(), str1.end());
+    cout << "str1: " << str1 << endl;
+    cout << "str2: " << str2 << endl;
+    reverse_copy(str1.begin(), str1.end(), str2.begin());
+    cout << "str2: " << str2 << endl;
+
+    reverse(str2.begin() + 2, str2.begin() + 8); //字符串str2部分逆向
+    cout << "str2: " << str2 << endl;
+    copy(str2.begin() + 2, str2.begin() + 8, ostream_iterator<char>(cout));
+    cout << endl;
+
+    sort(str1.begin(), str1.end(),greater<char>());
+    cout << "str1: " << str1 << endl;
+    str1.swap(str2);
+
+    cout << str1 << " " << str2 << endl;
+
+    cout << ( *find(str1.begin(), str1.end(), 'e') == 'e') << " " << ( *find(str1.begin(), str1.end(),'0') == '0') << endl;
+
+}
+
+int main()
+{
+    test3();
+
+    system("pause");
+    return 0;
+}
