2837406474495c973e90d5f7a3eb61cf7cde4fef
修罗大人 <14485564807@qq.com>
2019-08-03 12:54:20 +0800

    string数组

---
+++ part1/new-delete.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double *p;
+    p = new double[3];
+    for(int i = 0;i < 3;i++)
+    {
+        cin >> *(p + i);
+    }
+
+    for (int i = 0;i < 3;i++)
+    {
+        cout << *(p+i) << " ";
+    }
+
+    delete p;
+
+    system("pause");
+    return 0;
+}
+
--- part2/StringDemo.cpp
+++ part2/StringDemo.cpp
@@ -59,9 +59,29 @@ void test3()
 
 }
 
+void test4()
+{
+    string str[] = {"We are here!","Where are you?", "welcome!"};
+    for (int i = 0; i < 3; i++)
+    {
+        copy(str[i].begin(), str[i].end(), ostream_iterator<char>(cout));
+        cout << endl;
+    }
+    
+    str[0].swap(str[2]);
+    str[0].swap(str[1]);
+    for (int i = 0; i < 3; i++)
+    {
+        cout << str[i] << endl;
+    }
+    
+
+}
+
+
 int main()
 {
-    test3();
+    test4();
 
     system("pause");
     return 0;
