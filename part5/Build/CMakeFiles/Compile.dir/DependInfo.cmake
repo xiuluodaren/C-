6fb639fbb70123034dbae32697c0654c6fe1c46e
修罗大人 <14485564807@qq.com>
2019-08-11 11:57:50 +0800

    多文件

---
+++ part5/Build/CMakeFiles/Compile.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/C++Developer/workspace/part5/Find.cpp" "D:/C++Developer/workspace/part5/Build/CMakeFiles/Compile.dir/Find.cpp.obj"
+  "D:/C++Developer/workspace/part5/equation.cpp" "D:/C++Developer/workspace/part5/Build/CMakeFiles/Compile.dir/equation.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ part5/Build/CMakeFiles/Compile.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../Bin/Compile.exe"
+  "../Bin/Compile.exe.manifest"
+  "../Bin/Compile.pdb"
+  "../Bin/libCompile.dll.a"
+  "CMakeFiles/Compile.dir/Find.cpp.obj"
+  "CMakeFiles/Compile.dir/equation.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Compile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
